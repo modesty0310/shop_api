5ff8b3579892440291a2bcaf6dd27dfd84676b18
modesty0310 <rlatprua4@gmail.com>
2021-09-23 21:32:25 +0900

    디비 세팅

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 mongoose.connect(process.env.DB_URL)
   .then(console.log("DB Connected"))
