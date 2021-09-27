8be2a49fc144770955a7438668efaf53ea893b4d
modesty0310 <rlatprua4@gmail.com>
2021-09-27 20:35:05 +0900

    회원가입 기능 구현

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const CrytoJS = require("crypto-js")
+const CryptoJS = require("crypto-js")
 
+// 회원가입
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username : req.body.username,
     email : req.body.email,
-    password : CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC),
+    password : CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
   });
   console.log(req.body)
   try {
@@ -16,5 +17,7 @@ router.post('/register', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// 로그인
 
 module.exports = router;
