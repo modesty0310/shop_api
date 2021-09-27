fe003922d4cff600bcad71958b9ec17815da8005
modesty0310 <rlatprua4@gmail.com>
2021-09-27 20:13:05 +0900

    유저 등록 비밀번호 해시화 전

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//router
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+
 mongoose.connect(process.env.DB_URL)
   .then(console.log("DB Connected"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('./user', userRouter);
+app.use('./auth', authRouter);
 
 
 app.listen(process.env.PORT || 5000, () => {
