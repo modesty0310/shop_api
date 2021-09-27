84cd7b31ca8f72f7b34d2ce38a5178c4510c11ab
modesty0310 <rlatprua4@gmail.com>
2021-09-27 20:30:27 +0900

    crytojs를 활용한 비밀번호 해시화

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CrytoJS = require("crypto-js")
 
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username : req.body.username,
     email : req.body.email,
-    password : req.body.password,
+    password : CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC),
   });
   console.log(req.body)
   try {
