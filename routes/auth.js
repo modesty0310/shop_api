7e487869a4dfe65f4a840dda7219f94430b712a6
modesty0310 <rlatprua4@gmail.com>
2021-09-27 20:56:51 +0900

    jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const CryptoJS = require("crypto-js")
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // 회원가입
 router.post('/register', async (req, res) => {
@@ -19,5 +20,30 @@ router.post('/register', async (req, res) => {
 });
 
 // 로그인
+router.get('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+
+    !user && res.status(401).json("틀린 아이디 입니다.");
+
+    const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    const Originalpassword = hashPassword.toString(CryptoJS.enc.Utf8);
+
+    Originalpassword !== req.body.password && res.status(401).json('틀린 비밀번호 입니다.');
+
+    const accessToken = jwt.sign({
+      id: user._id,
+      isAdmin: user.isAdmin
+    }, 
+    process.env.JWT_SEC,
+    {expiresIn:"3d"}
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({...others, accessToken});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
