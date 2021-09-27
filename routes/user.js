a64c8e24b6d2d3111c653c0f3f3b89977900cdaf
modesty0310 <rlatprua4@gmail.com>
2021-09-27 21:32:59 +0900

    postman 무한 로딩 중

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
+const {verifToken, verifTokenAndAuthorization} = require('./verifyToken');
 
+// 수정
+router.put('/:id', verifTokenAndAuthorization, async (req, res) => {
+  if(req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password, 
+      process.env.PASS_SEC
+      ).toString();
+  };
 
+  console.log(req.body);
+
+  try {
+    const uqdateUser = await User.findByIdAndUpdate(
+      req.params.id, 
+      {
+        $set: req.body
+      }, 
+        {new:true}
+    );
+    res.status(200).json(uqdateUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifToken = (req,res,next) => {
+  const authHeader = req.headers.token;
+  if(authHeader){
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SEC), (err, user) => {
+      if(err) res.status(401).json("권한이 없습니다.");
+      req.user = user;
+      next();
+    };
+  }else{
+    return res.status(401).json("권한이 없습니다.");
+  }
+};
+
+const verifTokenAndAuthorization = (req, res, next) => {
+  verifToken(req,res, () => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+      next();
+    }else{
+      res.status(403).json("권한이 없습니다.")
+    }
+  })
+}
+
+module.exports = {verifToken, verifTokenAndAuthorization};
