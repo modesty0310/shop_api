2a7f2150a25d525c0836c355f0aab0a322a53aef
modesty0310 <rlatprua4@gmail.com>
2021-09-28 21:19:27 +0900

    사용자 생성 달에 따라 값가져오기

--- routes/user.js
+++ routes/user.js
@@ -57,5 +57,30 @@ router.get('/', verifTokenAndAdmin, async(req, res) => {
   }
 });
 
+// 사용자 상태 가져오기
+router.get('/stats', verifTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setUTCFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      {$match: {createdAt : {$gte: lastYear}}},
+      {
+        $project: {
+          month : { $month : "$createdAt"}
+        }
+      },
+      {
+        $group : {
+          _id : "$month",
+          total: {$sum : 1}
+        }
+      }
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
